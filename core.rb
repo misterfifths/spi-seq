8d2dbbca9259254bf0a192e2fe19a3a5504da4d0
Tim Clem <tim.clem@gmail.com>
2024-05-23 16:09:09 -0700

    Add core.rb: same as core.rb but includes seq.

---
+++ core.rb
@@ -0,0 +1,7 @@
+# `require` doesn't do what we want inside of spi, so we have to resort to eval.
+# $SPI_EXTS_PATH must set externally before eval'ing this file.
+
+eval IO.read("#{$SPI_EXTS_PATH}/midi-utils.rb")
+eval IO.read("#{$SPI_EXTS_PATH}/seq.rb")
+eval IO.read("#{$SPI_EXTS_PATH}/bezier.rb")
+eval IO.read("#{$SPI_EXTS_PATH}/easings.rb")  # this depends on bezier
