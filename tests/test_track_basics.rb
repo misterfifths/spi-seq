91255d277e82350770abbfe75078820ec89687d0
Tim Clem <tim.clem@gmail.com>
2025-03-01 15:29:08 -0800

    tests/track_basics: Add tests of some very basic Track methods.

---
+++ tests/test_track_basics.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "test/unit"
+require_relative "../track"
+require_relative "track_test_helpers"
+
+# Test basic Track methods - simple methods, direct attr mutators, etc.
+class TrackInitTest < Test::Unit::TestCase
+  include TrackTestHelpers
+
+  def test_basic_methods
+    assert_equal T(:c4).num_slots, 1
+    assert_equal T([:c4, :d4]).num_slots, 2
+
+    assert_equal T(:c4).beat_length, 0.5
+    assert_equal T([:c4, :d4]).beat_length, 1
+    assert_equal T([:c4, :d4], granularity: :whole).beat_length, 8
+
+    assert T(:r).empty?
+    assert T([:r, :r]).empty?
+    refute T(:c4).empty?
+
+    assert T(:r).mono?
+    assert T(:c4).mono?
+    assert T([:c4, :d4]).mono?
+    refute T([:c4, [:d4, :e4]]).mono?
+
+    refute T(:r).poly?
+    refute T(:c4).poly?
+    refute T([:c4, :d4]).poly?
+    assert T([:c4, [:d4, :e4]]).poly?
+  end
+
+  def test_attr_mutators
+    assert_gt T(:c4).with_granularity(:whole), NoteLength::Whole, 1
+    assert_gt T(:c4, granularity: :half).with_granularity(:sixteenth), NoteLength::Sixteenth, 1
+
+    assert_gt T(:c4).with_rate(2), NoteLength::Eighth, 2
+    assert_gt T(:c4, timescale: 5).with_rate(0.5), NoteLength::Eighth, 0.5
+  end
+end
