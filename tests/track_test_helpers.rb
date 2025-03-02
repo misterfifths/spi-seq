34d88b10ef33d202c7f5458bb41c2de3e9f67271
Tim Clem <tim.clem@gmail.com>
2025-03-02 14:10:29 -0800

    tests/track_test_helpers: Small improvements.
    Print messages when assert_grid fails, and simplify the logic in
    equal_steps?.

--- tests/track_test_helpers.rb
+++ tests/track_test_helpers.rb
@@ -3,33 +3,32 @@
 module TrackTestHelpers
   def equal_steps?(step, stepish)
     if stepish.is_a?(Step)
-      return false unless step.note == stepish.note
-      return false unless step.gate == stepish.gate
-      return false unless step.vel == stepish.vel
-      return false unless step.prob.equal?(stepish.prob)
-    else  # something MIDINote-ish
-      return false unless step.note == stepish
-      return false unless step.tied?
-      return false unless step.vel == 127
-      return false unless step.prob.nil?
+      return step.note == stepish.note &&
+             step.gate == stepish.gate &&
+             step.vel == stepish.vel &&
+             step.prob.equal?(stepish.prob)
     end
 
-    true
+    # something MIDINote-ish
+    step.note == stepish &&
+      step.tied? &&
+      step.vel == 127 &&
+      step.prob.nil?
   end
 
   def assert_grid(track, slots)
-    assert_equal track.length, slots.length
+    assert_equal track.length, slots.length, "grid length mismatch between #{track.repr} and #{slots.inspect}"
 
     track.grid.each_with_index do |slot, slot_idx|
       target_slot = slots[slot_idx]
-      assert_equal slot.length, target_slot.length
+      assert_equal slot.length, target_slot.length, "slot #{slot_idx} length mismatch: expected #{slot.inspect}, got #{target_slot.inspect}, track: #{track.repr}"
 
       # Step order is not significant and may be changed by the initializer, so
       # we need to check each target step against all steps in the track's slot.
       candidates = slot.dup
-      target_slot.each_with_index do |step, i|
+      target_slot.each do |step|
         winning_idx = candidates.index { |candstep| equal_steps?(candstep, step) }
-        refute_nil winning_idx, "no Step in slot #{i} matched #{step.inspect}"
+        refute_nil winning_idx, "no Step in slot #{slot_idx} matched #{step.inspect}, track: #{track.repr}"
         candidates.delete_at(winning_idx)
       end
     end
