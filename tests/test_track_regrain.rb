21391d164a00c78b45111b70f80d53337049e108
Tim Clem <tim.clem@gmail.com>
2025-03-08 15:47:53 -0800

    tests/regrain: Add tests of Track regranularizers.

---
+++ tests/test_track_regrain.rb
@@ -0,0 +1,110 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "test/unit"
+require_relative "../track"
+require_relative "track_test_helpers"
+
+# Test Track's methods for regranularizing.
+class TrackRegrainTest < Test::Unit::TestCase
+  include TrackTestHelpers
+
+  def test_expand
+    t = T([S(:a1, gate: 0.25), S(:b1, gate: 0.5), S(:c1, gate: 0.75), :d1])
+
+    t = t.expand
+    assert_gt t, NoteLength::Sixteenth, 1
+    assert_grid t, [
+      [S(:a1, gate: 0.5)], [],
+      [:b1], [],
+      [:c1], [S(:c1, gate: 0.5)],
+      [:d1], [:d1]
+    ]
+
+    t = t.expand
+    assert_gt t, NoteLength::ThirtySecond, 1
+    assert_grid t, [
+      [:a1], [], [], [],
+      [:b1], [:b1], [], [],
+      [:c1], [:c1], [:c1], [],
+      [:d1], [:d1], [:d1], [:d1]
+    ]
+
+    t = t.expand
+    assert_gt t, NoteLength::SixtyFourth, 1
+
+    assert_raises { t.expand }
+
+
+    # Probabilities should only go to the second step if one expands to two
+    # slots.
+    t = T([S(:a1, gate: 0.5, prob: Prob.fill), S(:b1, prob: Prob.fill)])
+    t = t.expand
+    assert_grid t, [
+      [S(:a1, prob: Prob.fill)], [],
+      [:b1], [S(:b1, prob: Prob.fill)]
+    ]
+  end
+
+  def test_condense
+    t = T([[:a1], [], [], [],
+           [:b1], [:b1], [], [],
+           [:c1], [:c1], [:c1], [],
+           [:d1], [:d1], [:d1], [:d1]])
+
+    t = t.condense
+    assert_gt t, NoteLength::Quarter, 1
+    assert_grid t, [
+      [S(:a1, gate: 0.5)], [],
+      [:b1], [],
+      [:c1], [S(:c1, gate: 0.5)],
+      [:d1], [:d1]
+    ]
+
+    t = t.condense
+    assert_gt t, NoteLength::Half, 1
+    assert_grid t, [
+      [S(:a1, gate: 0.25)],
+      [S(:b1, gate: 0.5)],
+      [S(:c1, gate: 0.75)],
+      [:d1]
+    ]
+
+    t = t.condense
+    assert_gt t, NoteLength::Whole, 1
+
+    assert_raises { t.condense }
+
+
+    # Steps that only appear in odd-indexed slots disappear.
+    t = T([:a1, :b1, :c1]).condense
+    assert_grid t, [[S(:a1, gate: 0.5)], [S(:c1, gate: 0.5)]]
+
+
+    # When condensing two steps into one, the probability and velocity of the
+    # first are the ones that win.
+    t = T(
+      [S(:a1, prob: Prob.fill, vel: 64),
+       S(:a1, gate: 0.5, prob: Prob.every_other, vel: 99)]
+    )
+    t = t.condense
+    assert_grid t, [[S(:a1, gate: 0.75, prob: Prob.fill, vel: 64)]]
+  end
+
+  def test_regranularize
+    t = T([S(:a1, gate: 0.25), S(:b1, gate: 0.5), S(:c1, gate: 0.75), :d1])
+
+    t = t.regrain(:thirty_second)
+    assert_gt t, NoteLength::ThirtySecond, 1
+    assert_grid t, [
+      [:a1], [], [], [],
+      [:b1], [:b1], [], [],
+      [:c1], [:c1], [:c1], [],
+      [:d1], [:d1], [:d1], [:d1]
+    ]
+
+    t = t.regrain(:eighth)
+    assert_gt t, NoteLength::Eighth, 1
+    assert_grid t, [[S(:a1, gate: 0.25)], [S(:b1, gate: 0.5)], [S(:c1, gate: 0.75)], [:d1]]
+  end
+end
