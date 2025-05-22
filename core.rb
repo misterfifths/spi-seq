ccd6feb78d51d024e78c3cdbdfd6f89ec015f23a
Tim Clem <tim.clem@gmail.com>
2025-05-22 16:45:07 -0700

    core: Explicitly require anything that is likely to be used directly.

--- core.rb
+++ core.rb
@@ -1,18 +1,28 @@
 # frozen_string_literal: true
 
-require_relative "math/easings"
 require_relative "math/curves"
+require_relative "math/easings"
 
+require_relative "theory/arp"
 require_relative "theory/chord"
+require_relative "theory/interval"
+require_relative "theory/midinote"
+require_relative "theory/notelength"
 require_relative "theory/scale"
 
-require_relative "utils/misc_utils"
-require_relative "utils/midi_utils"
 require_relative "utils/fx_utils"
 require_relative "utils/live_loop_utils"
+require_relative "utils/midi_utils"
+require_relative "utils/misc_utils"
 
+require_relative "prob"
+
+require_relative "step"
 require_relative "track"
-require_relative "cctrack"
 require_relative "player"
+
+require_relative "ccstep"
+require_relative "cctrack"
 require_relative "ccplayer"
+
 require_relative "track_live_loop"
