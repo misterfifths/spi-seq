0ddfae512c67dd042e4233070697e9c7c1d721a3
Tim Clem <tim.clem@gmail.com>
2025-12-14 11:11:50 -0800

    tests: Ignore core.rb for coverage.

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -5,6 +5,7 @@ begin
   SimpleCov.start do
     # Not including utils/ here since that's almost entirely Sonic Pi stuff.
     track_files "{math/,theory/,}*.rb"
+    add_filter "core.rb"
   end
 rescue LoadError  # rubocop:disable Lint/SuppressedException
 end
