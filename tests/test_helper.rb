77d9731e91f57d64258e6a31a5a228753cd2f1ab
Tim Clem <tim.clem@gmail.com>
2026-04-12 12:17:37 -0700

    tests: Ignore some Sonic Pi-heavy files in the coverage report.

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -6,6 +6,9 @@ begin
     # Not including utils/ here since that's almost entirely Sonic Pi stuff.
     track_files "{math/,theory/,}*.rb"
     add_filter "core.rb"
+    add_filter "track_live_loop.rb"
+    add_filter "playerbase.rb"
+    add_filter "player.rb"
   end
 rescue LoadError  # rubocop:disable Lint/SuppressedException
 end
