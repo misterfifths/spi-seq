4998d91b060c52519f1e62e6fa331649b20adbb0
Tim Clem <tim.clem@gmail.com>
2026-04-26 11:58:00 -0700

    tests/helper: Tweak SimpleCov configuration.
    Use an explicit working directory for when tests are run from Sonic
    Pi. Always exclude utils, in case one happens to get require'd.
    Make groups for files.

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -2,9 +2,19 @@
 
 begin
   require "simplecov"
+
+  root_dir = File.expand_path("#{File.dirname(__FILE__)}/..")
+
   SimpleCov.start do
+    root(root_dir)
+
     # Not including utils/ here since that's almost entirely Sonic Pi stuff.
     track_files "{math/,theory/,}*.rb"
+    add_group("core") { |f| File.dirname(f.filename) == root_dir }
+    add_group "theory", "theory/"
+    add_group "math", "math/"
+    add_group "tests", "tests/"
+    add_filter "utils/"
     add_filter "core.rb"
     add_filter "track_live_loop.rb"
   end
