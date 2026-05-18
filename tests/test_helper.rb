24a6788dcf9740adcfec53c36fed12b6a73929ec
Tim Clem <tim.clem@gmail.com>
2026-05-17 22:04:03 -0700

    tests: Include live_loop_utils.rb in coverage.

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -8,14 +8,17 @@ begin
   SimpleCov.start do
     root(root_dir)
 
-    # Not including utils/ here since that's almost entirely Sonic Pi stuff.
-    track_files "{math/,theory/,}*.rb"
+    track_files "{math/,theory/,utils/}*.rb"
     add_group("core") { |f| File.dirname(f.filename) == root_dir }
     add_group "theory", "theory/"
+    add_group "utils", "utils/"
     add_group "math", "math/"
     add_group "tests", "tests/"
-    add_filter "utils/"
     add_filter "core.rb"
+    # Most of utils/ is not very testable since it's very niche Sonic Pi stuff.
+    add_filter "utils/midi_utils.rb"
+    add_filter "utils/misc_utils.rb"
+    add_filter "utils/stop_hooks.rb"
   end
 rescue LoadError  # rubocop:disable Lint/SuppressedException
 end
