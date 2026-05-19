1ac191858c535cb48f579d7ec9ee889b7e668921
Tim Clem <tim.clem@gmail.com>
2026-05-18 17:31:24 -0700

    tests/run: Use a directory collector rather than require'ing all tests.

--- tests/run.rb
+++ tests/run.rb
@@ -1,81 +1,59 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative "../extapi"
 require_relative "test_helper"
 
-require_relative "test_extapi"
-require_relative "test_midinote"
-require_relative "test_arp"
-require_relative "test_step"
-require_relative "test_ccstep"
-require_relative "test_track_init"
-require_relative "test_track_basics"
-require_relative "test_track_grid"
-require_relative "test_track_step"
-require_relative "test_track_regrain"
-require_relative "test_trackrecorder"
-require_relative "test_cctrack"
-require_relative "test_chord"
-require_relative "test_chordvoicing"
-require_relative "test_euclid"
-require_relative "test_interval"
-require_relative "test_notelength"
-require_relative "test_scale"
-require_relative "test_ccplayer"
-require_relative "test_player"
-require_relative "test_track_live_loop"
-require_relative "test_mutable_live_loop"
-require_relative "test_prob"
-require_relative "test_accum"
-require_relative "test_curves_easings"
+BASE_DIR = File.expand_path("#{File.dirname(__FILE__)}/..")
+TEST_DIR = File.join(BASE_DIR, "tests")
+exit Test::Unit::AutoRunner.run(true, TEST_DIR) unless ExtApi.in_sonic_pi?
 
-if ExtApi.in_sonic_pi?
-  # To run the tests from inside Sonic Pi, call `init_spi_seq`, then require
-  # this file, then call `run_tests("path/to/output.log")`. E.g.:
-  #
-  # require "~/spi-seq/core"
-  # init_spi_seq
-  # require "~/spi-seq/tests/run"
-  # run_tests("~/spi-seq-tests.log")
-  #
-  # Note that since Sonic Pi maintains a single persistent Ruby context per
-  # launch, if you edit a test or any Ruby file you will have to quit and reopen
-  # Sonic Pi for the the changes to take effect.
-  #
-  # Also note that some tests may overwrite the implementations of Sonic Pi
-  # methods and thus break playback from spi-seq. Do not expect the Sonic Pi
-  # environment to be particularly usable after running the tests until you
-  # restart.
 
-  def run_tests(output_path)
-    require "test/unit/collector/descendant"
-    require "test/unit/ui/console/testrunner"
+# To run the tests from inside Sonic Pi, call `init_spi_seq`, then require this
+# file, then call `run_tests("path/to/output.log")`. E.g.:
+#
+# require "~/spi-seq/core"
+# init_spi_seq
+# require "~/spi-seq/tests/run"
+# run_tests("~/spi-seq-tests.log")
+#
+# Note that since Sonic Pi maintains a single persistent Ruby context per
+# launch, if you edit a test or any Ruby file you will have to quit and reopen
+# Sonic Pi for the the changes to take effect.
+#
+# Also note that some tests may overwrite the implementations of Sonic Pi
+# methods and thus break playback from spi-seq. Do not expect the Sonic Pi
+# environment to be particularly usable after running the tests until you
+# restart.
 
-    collector = Test::Unit::Collector::Descendant.new
-    suite = collector.collect("spi-seq")
-    ExtApi.puts("Found #{suite.tests.count} test case classes")
+def run_tests(output_path)
+  require "test/unit/collector/dir"
+  require "test/unit/ui/console/testrunner"
 
-    buggy_test_classes = [TrackLiveLoopTest, MutableLiveLoopTest]
-    subsuites_to_remove = []
-    suite.tests.each do |subsuite|
-      first_case = subsuite.tests.first
-      if buggy_test_classes.include?(first_case.class)
-        subsuites_to_remove << subsuite
-        ExtApi.puts("Skipping #{first_case.class}: known to be buggy in Sonic Pi")
-      end
+  collector = Test::Unit::Collector::Dir.new
+  suite = collector.collect(TEST_DIR)
+  ExtApi.puts("Found #{suite.tests.count} test case classes")
+
+  buggy_test_classes = [TrackLiveLoopTest, MutableLiveLoopTest]
+  subsuites_to_remove = []
+  suite.tests.each do |subsuite|
+    first_case = subsuite.tests.first
+    if buggy_test_classes.include?(first_case.class)
+      subsuites_to_remove << subsuite
+      ExtApi.puts("Skipping #{first_case.class}: known to be buggy in Sonic Pi")
     end
-    subsuites_to_remove.each { |subsuite| suite.delete(subsuite) }
+  end
+  subsuites_to_remove.each { |subsuite| suite.delete(subsuite) }
 
-    Test::Unit::UI::Console::TestRunner.run(suite, {
-      use_color: false,
-      progress_style: :mark,
-      output: File.open(File.expand_path(output_path), "w")
-    })
+  Test::Unit::UI::Console::TestRunner.run(suite, {
+    use_color: false,
+    progress_style: :mark,
+    output: File.open(File.expand_path(output_path), "w")
+  })
 
-    if suite.passed?
-      ExtApi.puts("Tests passed!")
-    else
-      ExtApi.puts("There were test failures; see the log")
-    end
+  if suite.passed?
+    ExtApi.puts("Tests passed!")
+  else
+    ExtApi.puts("There were test failures; see the log")
   end
 end
